a369dc4c1d886890fe23c7a60e9e0dc18384da6e
João Cerqueira <oss@cerqueira.io>
2021-09-09 11:33:47 +0100

    deps: added http-link-header to parse link headers

--- deps.ts
+++ deps.ts
@@ -3,3 +3,6 @@ export type {
   operations as GHOperations,
   paths as GHEndpoints,
 } from "./gen/openapi.d.ts";
+
+// skypack.dev/http-link-header
+export { default as HttpLinkHeader } from "https://cdn.skypack.dev/http-link-header";
